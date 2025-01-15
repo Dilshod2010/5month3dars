b7073cebacd44b19632c590295f2262576c89117
Dilshod2010 <dilshodjon.fahriddinov@gmail.com>
2025-01-15 19:34:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let randomNumber1 = Math.floor(Math.random() * 100); 
+let randomNumber2 = Math.floor(Math.random() * 100); 
+
+
+let multipliedNumber1 = randomNumber1 * 2;
+let multipliedNumber2 = randomNumber2 * 2;
+
+
+let dividedNumber1 = multipliedNumber1 / 2;
+let dividedNumber2 = multipliedNumber2 / 2;
+
+
+alert(`Random sonlar: ${randomNumber1} va ${randomNumber2}\n` + 
+      `Ko'paytirilgan sonlar: ${multipliedNumber1} va ${multipliedNumber2}\n` + 
+      `Bo'lingan sonlar: ${dividedNumber1} va ${dividedNumber2}`);
+
